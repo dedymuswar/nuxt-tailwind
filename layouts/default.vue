9fe13604c3710fee3b200d11bc301a5cd6847fc0
dedymuswar <dedymuswar@gmail.com>
2020-12-24 20:46:51 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar.vue"
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    'sans': ['ui-sans-serif', 'system-ui'],
+    'serif': ['ui-serif', 'Georgia'],
+    'mono': ['ui-monospace', 'SFMono-Regular'],
+   },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
